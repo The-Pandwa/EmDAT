bdbf35df44771fbff92b0270f84c260671cc34a6
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-08-21 12:42:39 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 df = pd.read_csv(r'public_emdat.csv')
 
@@ -10,3 +11,18 @@ st.map(geo,
     latitude='Latitude',
     longitude='Longitude'
     )
+
+# Ajouter une colonne pour les infobulles (par exemple, une description ou un autre champ)
+geo['info'] = "Point (" + geo['Latitude'].astype(str) + ", " + geo['Longitude'].astype(str) + ")"
+
+# Créer la couche de points avec les infobulles
+layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=geo,
+    get_position='[Longitude, Latitude]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=200,
+    pickable=True,
+    auto_highlight=True,
+    tooltip=True,
+)
