932de8771f878935cc25f3dd44b7a5fb247075d3
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-08-21 12:38:59 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,10 +3,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv(r'public_emdat.csv')
-df
 
 geo = df[df['Latitude'].notna()][['Latitude', 'Longitude']]
-geo
 
 st.map(geo,
     latitude='Latitude',
