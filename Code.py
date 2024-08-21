6a0fc0a4dce31eb371da243a799c1280a2fa4236
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-08-21 12:52:46 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -11,36 +11,3 @@ st.map(geo,
     latitude='Latitude',
     longitude='Longitude'
     )
-
-# Ajouter une colonne pour les infobulles (par exemple, une description ou un autre champ)
-geo['info'] = "Point (" + geo['Latitude'].astype(str) + ", " + geo['Longitude'].astype(str) + ")"
-
-# Créer la couche de points avec les infobulles
-layer = pdk.Layer(
-    'ScatterplotLayer',
-    data=geo,
-    get_position='[Longitude, Latitude]',
-    get_color='[200, 30, 0, 160]',
-    get_radius=200,
-    pickable=True,
-    auto_highlight=True,
-    tooltip=True,
-)
-
-# Configurer la vue de la carte
-view_state = pdk.ViewState(
-    latitude=geo['Latitude'].mean(),
-    longitude=geo['Longitude'].mean(),
-    zoom=2,
-    pitch=100,
-)
-
-# Créer la carte avec pydeck
-r = pdk.Deck(
-    layers=[layer],
-    initial_view_state=view_state,
-    tooltip={"text": "{info}"}
-)
-
-# Afficher la carte avec Streamlit
-st.pydeck_chart(r)
