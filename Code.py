9663919ffc4508a1a850f0ca8da08e81eabef6e1
The-Pandwa <152164692+The-Pandwa@users.noreply.github.com>
2024-08-21 13:29:54 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -17,5 +17,15 @@ st.map(geo,
 point = geo.iloc[0:1]
 point
 
-folium.map(location = point)
+# Créer une carte Folium centrée sur le point sélectionné
+m = folium.Map(location=[point['Latitude'].values[0], point['Longitude'].values[0]], zoom_start=10)
+
+# Ajouter un marqueur pour ce point
+folium.Marker(
+    location=[point['Latitude'].values[0], point['Longitude'].values[0]],
+    popup="First Point"
+).add_to(m)
+
+# Afficher la carte Folium dans Streamlit
+st_folium(m, width=700, height=500)
 
